cb1a6837c6e65ba4b698feab0217d0c9a1f3fe60
cst7785 <cestevens95@gmail.com>
2022-05-14 12:54:36 -0400

    Started implementing drawing on the canvas so that a user can draw their own letter.

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,5 +1,43 @@
+import {useEffect, useRef} from 'react'
+
+let drawing = false;
+
+
+
 export default function Canvas() {
+    
+    const canvasRef = useRef(null);
+    let contextRef = useRef(null);
+    useEffect(()=>{
+        const canvas = canvasRef.current;
+        contextRef.current = canvas.getContext('2d');
+        drawSettings();
+        // contextRef.current.fillRect(20,20,50,50)
+    },[])
+    
+    function drawSettings() {
+        contextRef.lineWidth = 5;
+        contextRef.lineCap = "round";
+    }
+    function startDrawing(e) {
+        console.log("Start of drawing")
+        drawing = true;
+        draw(e);    
+    }
+    function stopDrawing() {
+        console.log("End of drawing")
+        drawing = false;
+        contextRef.beginPath()
+    }
+    function draw(e) {
+        if (!drawing) return;
+        contextRef.current.lineTo(e.clientX, e.clientY);
+        contextRef.current.stroke();
+        contextRef.current.beginPath();
+        contextRef.moveTo(e.clientX, e.clientY)
+    }
+    
     return (
-        <canvas class="canvas"></canvas>
+        <canvas class="canvas" ref={canvasRef} onMouseDown={startDrawing} onMouseUp={stopDrawing} onMouseMove={draw}></canvas>
     )
 }
